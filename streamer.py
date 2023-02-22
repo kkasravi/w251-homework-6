61f82ce7a7d40d7b14dddc1fdfbd60983af36b7e
Kam Kasravi <kamkasravi@gmail.com>
2023-02-22 14:13:16 -0800

    snapshot

--- streamer.py
+++ streamer.py
@@ -2,16 +2,20 @@ import numpy as np
 import cv2
 
 # use gstreamer for video directly
-#camSet='v4l2src device=/dev/video0 ! videoconvert ! video/x-raw, format=BGR, width=1280, height=720, framerate=30/1 ! appsink sync=false'
-#camSet='v4l2src device=/dev/video0 ! videoconvert ! autovideosink sync=false -e'
 camSet='v4l2src device=/dev/video0 ! videoconvert ! appsink sync=false'
 cap= cv2.VideoCapture(camSet)
 
-#cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = 'Hello!'
+    org = (50, 50)
+    fontScale = 1
+    color = (255, 255, 255)
+    thickness = 2
+    cv2.putText(frame, text, org, font, fontScale, color, thickness, cv2.LINE_AA)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
